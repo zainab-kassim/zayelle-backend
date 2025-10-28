c7cdb622ce6ff4afcd65ffb213b04d54afd1cd7e
Zainab Kassim Temitope <kassimzainab11@gmail.com>
2025-10-28 13:33:26 +0100

    Product Controller implemented

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,3 +1,14 @@
 import { Products } from "../models/product.model";
 
 
+export const  GetProducts = ( async ( req,res)=>{
+    const products = await Products.find()
+     console.log(products)
+
+})
+
+export const GetProductsBySlug = (async (req,res)=>{
+    const slug = req.body
+    const product= await Products.findOne({slug})
+    console.log(product)
+})
